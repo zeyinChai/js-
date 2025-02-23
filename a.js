bcc2cfc6adbd006183fd121400338dfed8af6f8f
zeyinChai <18991877102@163.com>
2025-02-24 00:07:42 +0800

    feat: init

--- a.js
+++ a.js
@@ -1,2 +1,34 @@
-const data  = require('./index')
-console.log(data);
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  insert(node){
+    if(!this.root){
+        this.root = node
+    }else{
+        this.insertNode(this.root,node)
+    }
+  }
+  insertNode(root,node){
+    if(root.val > node){
+        
+    }else{
+
+    }
+  }
+}
+const tree = new Tree()
+
+const arr = [3,9,20,null,null,15,7]
+
+arr.forEach((item)=>{
+    tree.insert(item)
+})
